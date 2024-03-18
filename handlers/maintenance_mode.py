2f78f91755324594c5749e5cf4e7df17ff129291
Dmitry <dima.captan@yandex.ru>
2024-03-18 19:02:32 +0300

    added filter for callback_querry

--- handlers/maintenance_mode.py
+++ handlers/maintenance_mode.py
@@ -1,14 +1,22 @@
 from aiogram import Router, F
 from aiogram.filters import MagicData
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 maintenance_router = Router()
 maintenance_router.message.filter(MagicData(F.maintenance_mode.is_(True)))
 
 
 @maintenance_router.message()
-async def maintenance(message: Message) -> None:
+async def maintenance_message(message: Message) -> None:
     '''
     A handler for the maintenance mode
     '''
     await message.answer('В настоящее время бот находится в режиме технического обслуживания\u00A0🔧')
+
+
+@maintenance_router.callback_query()
+async def maintenance_callback_query(callback: CallbackQuery) -> None:
+    '''
+    A handler for the maintenance mode
+    '''
+    await callback.message.answer('В настоящее время бот находится в режиме технического обслуживания\u00A0🔧')

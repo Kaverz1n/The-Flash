0613fff4eca407bf7526bf4e153dd9bc8d3120dd
Dmitry <dima.captan@yandex.ru>
2024-03-23 13:29:41 +0300

    added a new button and solved a problem with an admin button

--- keyboards/for_start.py
+++ keyboards/for_start.py
@@ -12,7 +12,8 @@ def get_start_keyboard(user_id: int, admins_telegram_ids: list) -> InlineKeyboar
         '🚚\u00A0ОФОРМИТЬ ЗАКАЗ\u00A0📦': 'to_order', '🤔\u00A0КАК ЗАКАЗАТЬ❓': 'how_to_order',
         '📬\u00A0ВОЗВРАТ ТОВАРА\u00A0📨': 'cancel_order', '🦹‍♀\u00A0️ПРОФИЛЬ\u00A0😌': 'profile',
         '💵\u00A0КАЛЬКУЛЯТОР СТОИМОСТИ\u00A0💴': 'price_calculator', '☀️\u00A0О КОМПАНИИ\u00A0🌈': 'about_company',
-        '💌\u00A0ОТВЕТЫ НА ВОПРОСЫ\u00A0💬': 'answers_to_questions', '🌀\u00A0ПЕРЕЗАПУСК БОТА\u00A0🤖': 'restart_bot'
+        '💌\u00A0ОТВЕТЫ НА ВОПРОСЫ\u00A0💬': 'answers_to_questions', '🧑‍💻\u00A0ТЕХПОДДЕРЖКА\u00A0👩‍💻': 'tech_support',
+        '🌀\u00A0ПЕРЕЗАПУСК БОТА\u00A0🤖': 'restart_bot'
     }
 
     keyboard_builder = InlineKeyboardBuilder()
@@ -20,8 +21,8 @@ def get_start_keyboard(user_id: int, admins_telegram_ids: list) -> InlineKeyboar
     for text, callback_data in buttons_data.items():
         keyboard_builder.button(text=text, callback_data=callback_data)
 
-    if user_id not in admins_telegram_ids:
-        keyboard_builder.button(text='👨‍💼\u00A0АДМИНКА\u00A0👨‍💼', callback_data='to_admin_panel')
+    if user_id in admins_telegram_ids:
+        keyboard_builder.button(text='🥷\u00A0АДМИНКА\u00A0🥷', callback_data='to_admin_panel')
 
     keyboard_builder.adjust(1)
 

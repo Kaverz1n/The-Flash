2e60233fa499938ae82823c706651d3d9a3186b9
Dmitry <dima.captan@yandex.ru>
2024-03-23 13:28:31 +0300

    added callback_query filter

--- handlers/maintenance_mode.py
+++ handlers/maintenance_mode.py
@@ -4,6 +4,7 @@ from aiogram.types import Message, CallbackQuery
 
 maintenance_router = Router()
 maintenance_router.message.filter(MagicData(F.maintenance_mode.is_(True)))
+maintenance_router.callback_query.filter(MagicData(F.maintenance_mode.is_(True)))
 
 
 @maintenance_router.message()

87c0abca4ba7df3bf587e741d9329ddfcc86a89e
Dmitry <dima.captan@yandex.ru>
2024-04-16 12:25:43 +0300

    deleted maintenance_mode from Dispatcher and added new routers

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 from handlers import maintenance_mode, start, about_company, answers_to_questions, price_calculator, profile, \
-    different_handlers, to_order
+    different_handlers, to_order, how_to_order, cancel_order, admin_panel
 
 
 async def main() -> None:
@@ -19,15 +19,18 @@ async def main() -> None:
     load_dotenv('.env')
 
     bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode='HTML')
-    dispatcher = Dispatcher(maintenance_mode=False)
+    dispatcher = Dispatcher()
 
     dispatcher.include_router(maintenance_mode.maintenance_router)
     dispatcher.include_router(start.router)
     dispatcher.include_router(to_order.router)
+    dispatcher.include_router(how_to_order.router)
+    dispatcher.include_router(cancel_order.router)
     dispatcher.include_router(about_company.router)
     dispatcher.include_router(answers_to_questions.router)
     dispatcher.include_router(price_calculator.router)
     dispatcher.include_router(profile.router)
+    dispatcher.include_router(admin_panel.router)
     dispatcher.include_router(different_handlers.router)
 
     await dispatcher.start_polling(bot)

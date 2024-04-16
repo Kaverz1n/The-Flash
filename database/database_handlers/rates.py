c6a40fb45ed37f0133eb7a4aaa583179c254dc6e
Dmitry <dima.captan@yandex.ru>
2024-04-16 12:27:30 +0300

    get_course_and_commission has been changed to get_rate_and_commission and added: update_rate and update_commission

--- database/database_handlers/rates.py
+++ database/database_handlers/rates.py
@@ -3,9 +3,9 @@ import asyncpg
 from database.database_settings.database_data import get_database_data
 
 
-async def get_course_and_commission() -> list:
+async def get_rate_and_commission() -> list:
     '''
-    An async function to get the CNY course and service commission
+    An async function to get the CNY rate and service commission
     '''
     database_data = get_database_data()
     connection = await asyncpg.connect(**database_data)
@@ -23,3 +23,33 @@ async def get_course_and_commission() -> list:
         rates_data = ['Не указан курс', 'Не указана комиссия']
 
     return rates_data
+
+
+async def update_rate(rate_value: float) -> None:
+    '''
+    An async function to update the CNY rate
+    '''
+    database_data = get_database_data()
+    connection = await asyncpg.connect(**database_data)
+
+    await connection.execute(
+        'UPDATE rates SET rate_value = $1 WHERE rate_code = \'CNY\'',
+        rate_value
+    )
+
+    await connection.close()
+
+
+async def update_commission(commission: float) -> None:
+    '''
+    An async function to update the service commission
+    '''
+    database_data = get_database_data()
+    connection = await asyncpg.connect(**database_data)
+
+    await connection.execute(
+        'UPDATE rates SET commission = $1 WHERE rate_code = \'CNY\'',
+        commission
+    )
+
+    await connection.close()

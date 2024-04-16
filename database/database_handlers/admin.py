adc7ada99fe1277abe5d762939c18574e28ad5b2
Dmitry <dima.captan@yandex.ru>
2024-04-16 12:23:32 +0300

    added insert_admin_data, delete_from_admins and select_admin_password funcs

--- database/database_handlers/admin.py
+++ database/database_handlers/admin.py
@@ -19,3 +19,57 @@ async def get_admins_telegram_ids() -> list:
     admins_telegram_ids = [admin_telegram_id[0] for admin_telegram_id in result]
 
     return admins_telegram_ids
+
+
+async def insert_admin_data(admin_telegram_id: int, admin_password: str) -> None:
+    '''
+    An async function to insert admin data into the admins table
+    :param admin_telegram_id: admin's telegram id
+    :param admin_password: admin's password
+    '''
+    database_data = get_database_data()
+    connection = await asyncpg.connect(**database_data)
+
+    await connection.execute(
+        'INSERT INTO admins (admin_telegram_id, admin_password) VALUES ($1, $2)',
+        admin_telegram_id, admin_password
+    )
+
+    await connection.close()
+
+
+async def delete_from_admins(admin_telegram_id: int) -> None:
+    '''
+    An async function to delete admin data from the admins table
+    :param admin_telegram_id: admin's telegram id
+    '''
+    database_data = get_database_data()
+    connection = await asyncpg.connect(**database_data)
+
+    await connection.execute(
+        'DELETE FROM admins WHERE admin_telegram_id = $1',
+        admin_telegram_id
+    )
+
+    await connection.close()
+
+
+async def select_admin_password(admin_telegram_id: int) -> None:
+    '''
+    An async function to select admin password
+    :param admin_telegram_id: admin's telegram id
+    '''
+    database_data = get_database_data()
+    connection = await asyncpg.connect(**database_data)
+
+    result = await connection.fetch(
+        'SELECT admin_password FROM admins'
+        ' WHERE admin_telegram_id = $1',
+        admin_telegram_id
+    )
+
+    admin_password = result[0][0]
+
+    await connection.close()
+
+    return admin_password

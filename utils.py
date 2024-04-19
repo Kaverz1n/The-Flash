f4f4be57f13d47d5a724a03e9f504c4f8bc57773
Dmitry <dima.captan@yandex.ru>
2024-04-19 11:10:18 +0300

    added create_order_inf_file

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import os
 
 from cdek.api import CDEKClient
+from openpyxl import Workbook
+from openpyxl.styles import Font
+
+from database.database_handlers.orders import get_orders_inf
 
 
 async def is_correct_address(city_post_code: str, delivery_address: str) -> bool:
@@ -23,3 +27,35 @@ async def is_correct_address(city_post_code: str, delivery_address: str) -> bool
                 is_corrected = True
     finally:
         return is_corrected
+
+
+async def create_order_inf_file() -> None:
+    '''
+    An async function to create an excel file with orders data
+    '''
+    orders_inf_list = await get_orders_inf()
+
+    with open(f'templates/excel_template.xlsx', 'w', encoding='UTF-8') as excel_file:
+        workbook = Workbook()
+        sheet = workbook.active
+
+        headers = [
+            'id заказа', 'ссылка', 'цена в юан.', 'цена в руб.', 'id фото', 'размер',
+            'telegram-id заказчика', 'telegram-id чата с заказчиком', 'статус заказа'
+        ]
+
+        bold_font = Font(bold=True)
+        for column_num, header in enumerate(headers, start=1):
+            cell = sheet.cell(row=1, column=column_num)
+            cell.value = header
+            cell.font = bold_font
+
+        for order_inf in orders_inf_list:
+            sheet.append([
+                order_inf[0], order_inf[1], order_inf[2], order_inf[3], order_inf[4],
+                order_inf[5], order_inf[6], order_inf[7], order_inf[8]
+            ])
+
+            workbook.save('templates/excel_template.xlsx')
+            workbook.close()
+

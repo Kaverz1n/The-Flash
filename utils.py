3f468e29adaa1a9dc56dd2a96cad3afd673ad55e
Dmitry <dima.captan@yandex.ru>
2024-04-22 16:25:43 +0300

    added a main func to create excel-files: create_inf_file

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ from cdek.api import CDEKClient
 from openpyxl import Workbook
 from openpyxl.styles import Font
 
-from database.database_handlers.orders import get_orders_inf
-
 
 async def is_correct_address(city_post_code: str, delivery_address: str) -> bool:
     '''
@@ -29,20 +27,15 @@ async def is_correct_address(city_post_code: str, delivery_address: str) -> bool
         return is_corrected
 
 
-async def create_order_inf_file() -> None:
+async def create_inf_file(data_list: list, headers: list) -> None:
     '''
-    An async function to create an excel file with orders data
+    An async function to create an excel file with data
+    :param data_list: a list with data
+    :param headers: names of the columns
     '''
-    orders_inf_list = await get_orders_inf()
-
-    with open(f'templates/excel_template.xlsx', 'w', encoding='UTF-8') as excel_file:
+    with open(f'templates/excel_template.xlsx', 'w', encoding='UTF-8'):
         workbook = Workbook()
         sheet = workbook.active
-
-        headers = [
-            'id заказа', 'ссылка', 'цена в юан.', 'цена в руб.', 'id фото', 'размер',
-            'telegram-id заказчика', 'telegram-id чата с заказчиком', 'статус заказа'
-        ]
 
         bold_font = Font(bold=True)
         for column_num, header in enumerate(headers, start=1):
@@ -50,12 +43,9 @@ async def create_order_inf_file() -> None:
             cell.value = header
             cell.font = bold_font
 
-        for order_inf in orders_inf_list:
-            sheet.append([
-                order_inf[0], order_inf[1], order_inf[2], order_inf[3], order_inf[4],
-                order_inf[5], order_inf[6], order_inf[7], order_inf[8]
-            ])
+        for data in data_list:
+            data_elements = [data_element for data_element in data]
+            sheet.append(data_elements)
 
-            workbook.save('templates/excel_template.xlsx')
+            workbook.save('templates/data.xlsx')
             workbook.close()
-
